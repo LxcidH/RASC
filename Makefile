64066651195dadba06395a0d050e75b601ede413
LxcidH <taylor20050825@gmail.com>
2025-08-30 04:01:53 +0100

    Snek :3

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
+# Copyright (c) <2025>, <Lxcid>
+# All rights reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 CC		= gcc
 CFLAGS	= -Wall -g -std=c99
 LDLIBS  = -lncurses
--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/*
+Copyright (c) <2025>, <Lxcid>
+All rights reserved.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
 #include <ncurses.h>
 #include <unistd.h>
 #include <string.h>
